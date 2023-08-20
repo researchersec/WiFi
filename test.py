00289a3c5fc4e651311675b8efef33ad20d8190b
researchersec <109282836+researchersec@users.noreply.github.com>
2023-08-20 04:00:23 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,34 @@
+import asyncio
 import socket
 
-def find_online_devices():
+async def check_online_device(ip):
+    try:
+        await asyncio.wait_for(
+            asyncio.get_event_loop().sock_connect(
+                (ip, 80)
+            ),
+            timeout=1
+        )
+        return ip
+    except (OSError, asyncio.TimeoutError):
+        return None
+
+async def find_online_devices():
+    tasks = []
     online_devices = []
-    # Replace with your actual code to find online devices
+
     for i in range(1, 255):
         ip = f'192.168.1.{i}'
-        try:
-            socket.create_connection((ip, 80), timeout=1)
-            online_devices.append(ip)
-        except OSError:
-            pass
+        tasks.append(check_online_device(ip))
+
+    online_results = await asyncio.gather(*tasks)
+    online_devices = [ip for ip in online_results if ip is not None]
+
     return online_devices
-  
-find_online_devices()
+
+async def main():
+    online_devices = await find_online_devices()
+    print(online_devices)
+
+if __name__ == '__main__':
+    asyncio.run(main())
